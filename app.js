2f63498c144ac674131b731315d2a26a0798be37
Francis Miyoba <francismiyoba@gmail.com>
2017-11-14 01:35:20 -0800

    Setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+const PORT = 4004
+
+// set view engine 
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
+app.listen(PORT, console.log(`server running on ${PORT}`))
